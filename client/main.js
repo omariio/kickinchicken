b6adfe3bb40af20cf1800d49f99090ba27b0dd9c
Tyler Smith <tylsmith@gmail.com>
2015-02-25 11:56:40 -0800

    fixed semicolon issue, quantity thing broken for now

--- client/main.js
+++ client/main.js
@@ -5,6 +5,6 @@ Meteor.startup(function () {
     // code to run on server at startup
     //console.log("started");
     if(!Session.get("cart")){
-      Session.set("cart", Items.find().fetch();)
+      Session.set("cart", Items.find().fetch())
     }
 });

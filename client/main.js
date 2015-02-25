93bc67a9e3e2e09332d0076f53692888ef7d62a2
Tyler Smith <tylsmith@gmail.com>
2015-02-25 14:41:25 -0800

    price total now displayed

--- client/main.js
+++ client/main.js
@@ -3,8 +3,7 @@ Meteor.subscribe("items");
 
 Meteor.startup(function () {
     // code to run on server at startup
-    //console.log("started");
     if(!Session.get("cart")){
-      Session.set("cart", Items.find().fetch())
+      Session.set("cart", []);
     }
 });
--- client/menu/menu.js
+++ client/menu/menu.js
@@ -5,48 +5,79 @@ Template.menu.helpers({
   },
   quantity: function(_id) {
     var cart = Session.get("cart");
-    var q = cart["item-" + _id];
+    var q = _.find(cart, function(i){
+      return i._id == _id;
+    });
     if(q)
-      return q;
+      return q.cartQuantity;
     else
       return 0;
   },
   cartItems: function(){
     var cart = Session.get("cart");
-    // _.forEach(Session.get("cart"), function(item){
-    //   //var id = item.id.replace("item-", "");
-    //   console.log(item);
-
-    // })
-    for (var key in cart) {
-      var id = key.replace("item-", "");
-      console.log(id);
-    }
-
-    return [];
+    _.filter(cart, function(i){
+      return (i.cartQuantity && i.cartQuantity != 0)
+    });
+    return cart;
+  },
+  total: function() {
+    var items = Template.menu.__helpers[" cartItems"]()
+    var total = 0;
+    _.forEach(items, function(item){
+      total += parseFloat(item.price) * item.cartQuantity;
+    });
+    return total;
+  },
+  totalWithTax: function() {
+    var total = Template.menu.__helpers[" total"]()
+    return (total * 1.075).toFixed(2);
   }
 });
 
 Template.menu.events({
   'click .up': function(event){
-    var id = event.target.id;
+    var id = event.target.id.replace("item-", "");
     var cart = Session.get("cart");
-    if(!cart[id]){
-      cart[id] = 0;
+    var item = _.find(cart, function(i){
+      return i._id == id;
+    });
+
+    if(!item){
+      if(!cart)
+        cart = [];
+
+      item = Items.findOne({_id: id});
+      cart.push(item);
     }
-    cart[id]++;
+
+    if(!item.cartQuantity){
+      item.cartQuantity = 0;
+    }
+    item.cartQuantity++;
 
     Session.set("cart", cart);
   },
   'click .down': function(event){
-    var id = event.target.id;
+    var id = event.target.id.replace("item-", "");
     var cart = Session.get("cart");
-    if(!cart[id]){
-      cart[id] = 0;
+    var item = _.find(cart, function(i){
+      return i._id == id;
+    });
+
+    if(!item){
+      if(!cart)
+        cart = [];
+      item = Items.findOne({_id: id});
+      cart.push(item);
     }
 
-    if(cart[id] > 0)
-      cart[id]--;
+    if(!item.cartQuantity){
+      item.cartQuantity = 0;
+    }
+
+    if(item.cartQuantity >= 1){
+      item.cartQuantity--;
+    }
 
     Session.set("cart", cart);
 

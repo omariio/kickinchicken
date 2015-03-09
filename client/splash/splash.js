a0591108e2091dc599cca76b29532b46a81d1ed3
pndpo <peterdepaulo@gmail.com>
2015-03-09 10:40:19 -0700

    the most current changes

--- client/splash/splash.js
+++ client/splash/splash.js
@@ -3,11 +3,11 @@ Template.splash.helpers({
 	openNow: function() {
 		var d = new Date;
 		var minutes = d.getMinutes()
-		var hoursAsMinutes = (d.getHours() * 60);
-		var timeNow = hoursAsMinutes + minutes;
+		var hoursAsMinutes = (d.getHours() * 60); // converts current hour to minutes
+		var timeNow = hoursAsMinutes + minutes; // adds the current minutes to hour
 
-		var timeOpen = 1260; // time in minutes since midnight
-		var timeClosed = 120; // time in minutes since midnight
+		var timeOpen = 1260; // time in minutes since midnight for open
+		var timeClosed = 120; // time in minutes since midnight for closed
 
 		var openNow = (timeOpen < timeNow || timeNow < timeClosed);
 		return openNow;

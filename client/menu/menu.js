b109f92b8d003a2b7c7f1913bc331f4efb0db5cf
Tyler Smith <tylsmith@gmail.com>
2015-03-27 19:34:44 -0700

    tweaks

--- client/menu/menu.js
+++ client/menu/menu.js
@@ -41,7 +41,6 @@ Template.menu.helpers({
 Template.menu.events({
   'click .menu-item-up': function(event){
     var id = event.target.id.replace("item-", "");
-    console.log(id);
     var cart = Session.get("cart");
     var item = _.find(cart, function(i){
       return i._id == id;
@@ -83,9 +82,9 @@ Template.menu.events({
     if(item.cartQuantity >= 1){
       item.cartQuantity--;
       if(item.cartQuantity === 0){
-        _.remove(cart, function(n)){
+        _.remove(cart, function(n){
           return n.quantity === 0;
-        }
+        });
       }
     }
 
@@ -101,5 +100,8 @@ Template.menu.events({
 
   'click .delete':function(event) {
     Meteor.call('destroyItem', this._id); // is there a safer way to do this?
+
+    // Best effort, we don't really care about the result.
+    S3.delete(this.imageUrl);
   }
 });
